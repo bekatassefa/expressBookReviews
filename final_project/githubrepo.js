1b147635ac555e4c3145e81e64ad5e7af95cd557
bekatassefa <155350307+bekatassefa@users.noreply.github.com>
2026-02-10 10:32:27 +0300

    Update githubrepo.js

--- final_project/githubrepo.js
+++ final_project/githubrepo.js
@@ -1,5 +1,19 @@
 {
+  "id": 924556123,
+  "name": "expressBookReviews",
   "full_name": "bekatassefa/expressBookReviews",
+  "private": false,
   "fork": true,
-  "parent": { "full_name": "ibm-developer-skills-network/expressBookReviews" }
+  "url": "https://api.github.com/repos/bekatassefa/expressBookReviews",
+  "html_url": "https://github.com/bekatassefa/expressBookReviews",
+  "parent": {
+    "name": "expressBookReviews",
+    "full_name": "ibm-developer-skills-network/expressBookReviews",
+    "html_url": "https://github.com/ibm-developer-skills-network/expressBookReviews"
+  },
+  "source": {
+    "name": "expressBookReviews",
+    "full_name": "ibm-developer-skills-network/expressBookReviews",
+    "html_url": "https://github.com/ibm-developer-skills-network/expressBookReviews"
+  }
 }
